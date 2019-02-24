c09c09769476d840f3130efa36c34d2939bda5a1
jackdaife <jackdaifei@126.com>
2019-02-24 22:47:42 +0800

    third chapter

--- app/src/main/java/com/example/fly/androidlearn/ThirdChapterActivity.java
+++ app/src/main/java/com/example/fly/androidlearn/ThirdChapterActivity.java
@@ -1,6 +1,9 @@
 package com.example.fly.androidlearn;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +21,8 @@ public class ThirdChapterActivity extends AppCompatActivity implements View.OnCl
 
     private ProgressBar thirdProgress;
 
+    private ProgressBar thirdProgress2;
+
     private boolean flag = true;
 
     @Override
@@ -31,11 +36,25 @@ public class ThirdChapterActivity extends AppCompatActivity implements View.OnCl
         Button thirdBtn2 = (Button) findViewById(R.id.third_btn_2);
         thirdBtn2.setOnClickListener(this);
 
+        Button thirdBtn3 = (Button) findViewById(R.id.third_btn_3);
+        thirdBtn3.setOnClickListener(this);
+
+        Button thirdBtn4 = (Button) findViewById(R.id.third_btn_4);
+        thirdBtn4.setOnClickListener(this);
+
+        Button thirdBtn5 = (Button) findViewById(R.id.third_btn_5);
+        thirdBtn5.setOnClickListener(this);
+
+        Button thirdBtn6 = (Button) findViewById(R.id.third_btn_6);
+        thirdBtn6.setOnClickListener(this);
+
         thirdEdit = (EditText) findViewById(R.id.third_edit);
 
         thirdImage = (ImageView) findViewById(R.id.third_image);
 
         thirdProgress = (ProgressBar) findViewById(R.id.third_progress);
+
+        thirdProgress2 = (ProgressBar) findViewById(R.id.third_progress2);
     }
 
     @Override
@@ -51,7 +70,6 @@ public class ThirdChapterActivity extends AppCompatActivity implements View.OnCl
                     }
                     break;
                 case R.id.third_btn_2:
-                    thirdProgress.setVisibility(View.VISIBLE);
                     if (flag) {
                         thirdImage.setImageResource(R.drawable.img_2);
                         flag = false;
@@ -59,7 +77,49 @@ public class ThirdChapterActivity extends AppCompatActivity implements View.OnCl
                         thirdImage.setImageResource(R.drawable.img_1);
                         flag = true;
                     }
-                    thirdProgress.setVisibility(View.GONE);
+                    break;
+                case R.id.third_btn_3:
+                    if (View.VISIBLE == thirdProgress.getVisibility()) {
+                        thirdProgress.setVisibility(View.GONE);
+                    } else {
+                        thirdProgress.setVisibility(View.VISIBLE);
+                    }
+                    break;
+                case R.id.third_btn_4:
+                    int progress = thirdProgress2.getProgress();
+                    if (progress < 100) {
+                        progress += 10;
+                    } else {
+                        progress = 0;
+                    }
+                    thirdProgress2.setProgress(progress);
+                    break;
+                case R.id.third_btn_5:
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(ThirdChapterActivity.this);
+                    dialog.setTitle("This is Dialog.");
+                    dialog.setMessage("Something important!!!");
+                    dialog.setCancelable(false);
+                    dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    dialog.show();
+                    break;
+                case R.id.third_btn_6:
+                    ProgressDialog progressDialog = new ProgressDialog(ThirdChapterActivity.this);
+
+                    progressDialog.setTitle("This is ProgressDialog.");
+                    progressDialog.setMessage("Loading...");
+                    progressDialog.setCancelable(true);
+                    progressDialog.show();
                     break;
                 default:
                     break;
